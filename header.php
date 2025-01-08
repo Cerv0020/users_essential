4d6410c8968d51193cfaee902084ecb5f64afc56
Cerv0020 <dannypintosd@gmail.com>
2025-01-08 21:11:15 +0000

    1st commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="container">
+        <div class="navbar">
+            <ul>
+                <li><a href="index.php" class="active">Listar</a></li>
+                <li><a href="adicionar.php">Adicionar</a></li>
+                <li><a href="remover.php">Remover</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php include_once 'head.php' ?>
+
+<body>
+    <?php include_once 'header.php' ?>
+    <script src="app.js" async defer></script>
+</body>
+
+</html>
